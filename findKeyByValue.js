0ec72f4d149802668accfd7b8ae2c767ff5a01e1
delalhagos <dalalj2011@gmail.com>
2023-01-11 22:17:51 -0500

    adding files

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:  ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !==  ${expected}`);
+  }
+};
+
+
+
+const findKeyByValue = function (show, value){
+for (const item of Object.keys(show)) {
+  if (show[item] === value){
+    return item
+  }
+  
+} 
+
+}
+
+
+// test cases
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
